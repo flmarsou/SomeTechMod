23b02759b1687cd6ada92d00a49f259b91d12ddd
flmarsou <marsouinflorent12@gmail.com>
2024-12-09 20:38:51 +0100

    Initial commit

---
+++ src/main/java/flo/sometechmod/SomeTechMod.java
@@ -0,0 +1,18 @@
+package flo.sometechmod;
+
+import net.fabricmc.api.ModInitializer;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class SomeTechMod implements ModInitializer
+{
+	public static final String MOD_ID = "sometechmod";
+	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+
+	@Override
+	public void onInitialize()
+	{
+		
+	}
+}
---
+++ src/main/java/flo/sometechmod/SomeTechModClient.java
@@ -0,0 +1,12 @@
+package flo.sometechmod;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class SomeTechModClient implements ClientModInitializer
+{
+	@Override
+	public void onInitializeClient()
+	{
+		
+	}
+}
